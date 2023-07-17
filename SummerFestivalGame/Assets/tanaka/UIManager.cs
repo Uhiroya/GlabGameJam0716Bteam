43e8543223a60992e3c5cea7459e370c96f9d827
tanakasora1203 <sora1042hym@gmail.com>
2023-07-17 11:45:05 +0900

    スコアと制限時間のUIです。

--- SummerFestivalGame/Assets/tanaka/UIManager.cs
+++ SummerFestivalGame/Assets/tanaka/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,19 +10,35 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField]
     Text textObj;
+    [SerializeField]
+    Text timeText;
 
     //int score = 0;
+
+
+    public int countdownMinutes = 3;
+    private float countdownSeconds;  
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //timeText = GetComponent<Text>();
+        countdownSeconds = countdownMinutes * 60;
     }
 
     // Update is called once per frame
     void Update()
     {
+        countdownSeconds -= Time.deltaTime;
+        var span = new TimeSpan(0, 0, (int)countdownSeconds);
+        timeText.text = span.ToString(@"mm\:ss");
 
+        if (countdownSeconds <= 0)
+        {
+            //‚O•b‚É‚È‚Á‚½Žž‚Ìˆ—
+        }
+            
         ExpressMoney();
     }
 
